8eb1de0db6aabd93ce7df5954b3706f86c4eae51
Seth Gardner <seth.gardner.a@gmail.com>
2021-08-25 15:12:42 -0500

    finished limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,8 @@
+use employees;
+
+select distinct title from titles;
+
+select distinct last_name from employees order by last_name desc limit 10;
+
+select emp_no from salaries order by salary desc limit 5 offset 45;
+
---
+++ order_by_exercises.sql
@@ -0,0 +1,11 @@
+use employees;
+
+select * from employees where first_name in ('Irena', 'Vidya', 'maya') and (gender = 'M') order by last_name;
+
+select * from employees where last_name like 'E%';
+
+select * from employees where last_name like '%q%';
+
+select  * from employees where last_name like 'e%' and( last_name like '%e');
+
+select * from employees where last_name like '%q%' and(last_name not like'%qu%')
